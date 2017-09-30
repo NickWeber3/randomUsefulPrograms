b7192fbb496be5adb0d12dfa0a7c0851d7751c55
Nick Weber <webenic3@gmail.com>
2017-09-30 13:31:11 -0400

    C++ program to find GCD between two integers

--- greatest_common_denominator.cpp
+++ greatest_common_denominator.cpp
@@ -1,8 +1,8 @@
-int greatest_common_denominator(int A, int B) {
-  int smallest = ((A < B) ? A : B);
+int greatest_common_denominator(int a, int b) {
+  int smallest = ((a < b) ? a : b);
     int greatest_common_dem = 1, increment = 2;
     while (increment <= smallest) {
-        if (A % increment == 0 and B % increment == 0) {
+        if (a % increment == 0 and b % increment == 0) {
             greatest_common_dem = increment;
         }
         ++increment;
